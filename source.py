8a99cd135a866fa0f79401a8b42926d0acc75a44
barearojo <barearojojuan@gmail.com>
2024-04-05 21:42:40 +0200

    preprocesado basico

--- source.py
+++ source.py
@@ -18,7 +18,7 @@ import numpy as np
 # Define la ruta al directorio que contiene el conjunto de datos de entrenamiento de imágenes
 training_dataset_path = "./data/training/training"
 # Define la ruta al directorio que contiene el conjunto de datos de validación de imágenes
-training_dataset_path = "./data/validation/validation"
+validation_dataset_path = "./data/validation/validation"
 
 
 mean = [0.4363, 0.4328, 0.3291]
@@ -40,11 +40,22 @@ training_resize = transform.Compose([
 
 # Define una secuencia de transformaciones para preprocesar las imágenes de validación
 validation_resize = transform.Compose([
-    # Redimensiona las imágenes a 224x224 píxeles
+    # Redimensiona las imágenes a 224x224 píxeles PUEDE AFECTAR A LA PERFORMANCE DEL MODELO
     transform.Resize((224, 224)),
     # Convierte las imágenes en tensores de PyTorch
-    transform.ToTensor()
+    transform.ToTensor(),
+    #normalización
+    transform.Normalize(torch.Tensor(mean),torch.Tensor(std))
 ])
+
+
+
+train_dataset = torchvision.datasets.ImageFolder( root=training_dataset_path, transform=training_resize)
+validation_dataset = torchvision.datasets.ImageFolder(root= validation_dataset_path, transform=validation_resize)
+
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=32, shuffle=True)
+validation_loader = torch.utils.data.DataLoader(dataset=validation_dataset, batch_size=32, shuffle=False)
+
 
 
 
