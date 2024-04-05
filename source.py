6c2d3325e7c4fec07f105345bd5a6b24f3641bb0
barearojo <barearojojuan@gmail.com>
2024-04-05 17:13:47 +0200

    std mean

--- source.py
+++ source.py
@@ -5,6 +5,8 @@ import os
 import torch
 
 # Importa torchvision, que proporciona herramientas y conjuntos de datos para la visión por computadora
+import torch.utils
+import torch.utils.data
 import torchvision
 
 # Importa el módulo de transformaciones de torchvision y lo renombra como 'transform'
@@ -29,4 +31,39 @@ train_dataset = torchvision.datasets.ImageFolder(
     transform=training_resize
 )
 
-print("aaa")
+# Crea un DataLoader para el conjunto de datos de entrenamiento
+# 'dataset' especifica el conjunto de datos a cargar
+# 'batch_size' define el tamaño del lote, es decir, cuántas muestras se cargarán en cada iteración
+# 'shuffle' indica si se deben barajar los datos antes de cargarlos en lotes
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset, batch_size=32, shuffle=False)
+
+# Define una función para calcular la media y la desviación estándar de un conjunto de datos
+def get_mean_std(loader):
+    # Inicializa la suma de la media y la desviación estándar
+    mean = 0
+    std = 0
+    # Inicializa el contador total de imágenes
+    total_images_count = 0
+    # Itera sobre los lotes del DataLoader
+    for images, _ in loader:
+        # Obtiene el tamaño del lote de imágenes
+        images_count_batch = images.size(0)
+        # Redimensiona las imágenes para que tengan la forma (batch_size, num_channels, num_pixels)
+        images = images.view(images_count_batch, images.size(1), -1)
+        # Calcula la suma de los valores de píxeles en el eje de los píxeles (dimensión 2) para calcular la media
+        mean += images.mean(2).sum(0)
+        # Calcula la suma de las desviaciones estándar de los valores de píxeles para calcular la desviación estándar
+        std += images.std(2).sum(0)
+        # Actualiza el contador total de imágenes
+        total_images_count += images_count_batch
+
+    # Calcula la media final dividiendo la suma acumulada por el total de imágenes
+    mean /= total_images_count
+    # Calcula la desviación estándar final dividiendo la suma acumulada por el total de imágenes
+    std /= total_images_count
+
+    return mean, std
+
+# Imprime la media y la desviación estándar del conjunto de datos de entrenamiento
+print(get_mean_std(train_loader))
+
