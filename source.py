953341a6ada8471679106c05dcb59b801bdd71d7
barearojo <barearojojuan@gmail.com>
2024-04-06 19:18:34 +0200

    Entrenamiento necesario

--- source.py
+++ source.py
@@ -74,14 +74,93 @@ print("Para entrenar la red neuronal se va a utilizar el siguiente componente ",
 
 
 def train_nn(model, train_loader, test_loader, criterion, optimizer, n_epochs):
-    return 3
+    device = set_device()
+
+    for epoch in range(n_epochs):
+        print("Iteración número", (epoch + 1))
+        model.train()
+        running_loss = 0.0
+        running_correct = 0.0
+        total = 0
+
+        for data in train_loader:
+            images, labels = data
+            images = images.to(device)
+            labels = labels.to(device)
+            total += labels.size(0)
+
+            optimizer.zero_grad()
+
+            outputs = model(images)
+
+            _, predicted = torch.max(outputs.data,1)
+
+            loss = criterion(outputs,labels)
+
+            loss.backward()
+
+            optimizer.step()
+
+            running_loss += loss.item()
+            running_correct += (labels==predicted).sum().item()
+
+        epoch_loss = running_loss/len(train_loader)
+        epoch_acc = 100.00 *  running_correct/ total
+        print("----- Training dataset got {} out of {} images correct ({}%). Con una pérdida de {}".format(running_correct, total, epoch_acc, epoch_loss))
+
+        evaluate_model(model,test_loader)
+    
+    print("Entrenamiento terminado")
+    return model
+
 
 def evaluate_model(model, test_loader):
-    return 4
+    model.eval()
+    predicted_correct_epoch = 0
+    total = 0
+    device = set_device()
+
+    with torch.no_grad():
+        for data in train_loader:
+            images, labels = data
+            images = images.to(device)
+            labels = labels.to(device)
+            total += labels.size(0)
+
+            optimizer.zero_grad()
+
+            outputs = model(images)
+
+            _, predicted = torch.max(outputs.data,1)
+            predicted_correct_epoch += (labels==predicted).sum().item()
+            epoch_acc = 100.00 *  predicted_correct_epoch/ total
+            print("----- Testing dataset got {} out of {} images correct ({}%)".format(predicted_correct_epoch, total, epoch_acc))
+
+
+
 
 #se ha elegido el 50 debido a que ofrece un buen balance entre eficiencia y precisión
-model_usado = models.resnet50(pretrained = True)
+model_usado = models.resnet18(pretrained = True)
 
+#numero de caracteristicas a tener en cuenta
+num_ftr = model_usado.fc.in_features
+
+#numero de distintas clases 
+number_classes = 10
+
+#prepara la matrices de propagración
+model_usado.fc = nn.Linear(num_ftr, number_classes)
+
+#le añadimos el device que vamos a usar
+model_con_device = model_usado.to(device)
+
+#funcion de perdida
+loss_fn = nn.CrossEntropyLoss()
+
+#elegimos optimizador
+optimizer = optim.SGD(model_con_device.parameters(), lr = 0.01, momentum=0.9, weight_decay=0.003)
+
+train_nn(model_con_device,train_loader,validation_loader,loss_fn,optimizer,100)
 
 
 
