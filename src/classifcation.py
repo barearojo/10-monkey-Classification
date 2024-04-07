2d2af7fcb804b693ca2bf5442112b54f74df8b8a
barearojo <barearojojuan@gmail.com>
2024-04-07 20:50:44 +0200

    classifcation empezado

--- src/classifcation.py
+++ src/classifcation.py
@@ -1,2 +1,25 @@
+import os
+import torchvision
 import torch
-import torcv
+import torchvision.transforms as transforms
+import PIL.Image as Image
+from utils import encontrar_mejor_modelo
+
+classes = [
+    "Mantled howler",
+    "Patas monkey",
+    "Bald uakari",
+    "Japanese macaque",
+    "Pygmy marmoset",
+    "White headed capuchin",
+    "Silvery marmoset",
+    "Common squirrel monkey",
+    "Black headed night monkey",
+    "Nilgiri langur"
+]
+
+ruta_modelo = "./models"
+
+mejor_modelo  = encontrar_mejor_modelo(ruta_modelo)
+
+print("El modelo que se va a usar es ", mejor_modelo)
--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,29 @@
 import torch
+import os
+import re
+
 def set_device():
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     return device
+
+
+def encontrar_mejor_modelo(ruta_carpeta):
+    mejor_modelo = None
+    mejor_puntaje = 0.0
+
+    # Recorre todos los archivos en la carpeta
+    for archivo in os.listdir(ruta_carpeta):
+        # Utiliza expresiones regulares para buscar el formato del nombre del archivo
+
+        patron = r"mejor_modelo_([\d.]+)\.pth"
+        coincidencia = re.match(patron, archivo)
+        print(archivo)
+        if coincidencia:
+            
+            puntaje = float(coincidencia.group(1))
+            if puntaje > mejor_puntaje:
+                mejor_puntaje = puntaje
+                mejor_modelo = archivo
+
+
+    return mejor_modelo
