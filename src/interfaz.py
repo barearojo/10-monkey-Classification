f90315f2d3c51076f6521463682f0d5283d4cbd9
barearojo <barearojojuan@gmail.com>
2024-04-09 15:43:13 +0200

    falta meter todos los monos y documentación

--- src/interfaz.py
+++ src/interfaz.py
@@ -8,16 +8,16 @@ from utils import encontrar_mejor_modelo
 from functions import classify
 
 classes = [
-    "Mantled howler",
-    "Patas monkey",
-    "Bald uakari",
-    "Japanese macaque",
-    "Pygmy marmoset",
-    "White headed capuchin",
-    "Silvery marmoset",
-    "Common squirrel monkey",
-    "Black headed night monkey",
-    "Nilgiri langur"
+    "Mantled_howler",
+    "Patas_monkey",
+    "Bald_uakari",
+    "Japanese_macaque",
+    "Pygmy_marmoset",
+    "White_headed_capuchin",
+    "Silvery_marmoset",
+    "Common squirrel_monkey",
+    "Black_headed_night_monkey",
+    "Nilgiri_langur"
 ]
 
 ruta_modelo = "./models"
@@ -38,37 +38,58 @@ def classify_image():
     file_path = filedialog.askopenfilename()
     if file_path:
         try:
-            image = Image.open(file_path)
+            image_usuario = Image.open(file_path)
             prediction = classify(mejor_modelo, image_resize, file_path)
             result_label.config(text=f"Predicción: {classes[prediction]}")
             # Mostrar la imagen cargada
-            display_image(image)
+            display_image(image_usuario)
+            # Mostrar la imagen correspondiente a la clase predicha
+            show_class_image(prediction)
         except Exception as e:
             messagebox.showerror("Error", f"Error al procesar la imagen: {str(e)}")
 
 def display_image(image):
     # Redimensionar la imagen para ajustarla al tamaño de visualización
-    image = image.resize((700, 700), Image.ANTIALIAS)
+    image = image.resize((500, 500), Image.ANTIALIAS)
     # Convertir la imagen a un formato compatible con Tkinter
     img = ImageTk.PhotoImage(image)
-    # Actualizar el widget de imagen
-    image_label.config(image=img)
-    image_label.image = img  # Mantener una referencia para evitar que se recolecte el objeto
+    # Actualizar el widget de imagen del usuario
+    user_image_label.config(image=img)
+    user_image_label.image = img  # Mantener una referencia para evitar que se recolecte el objeto
+
+def show_class_image(prediction):
+    # Obtener la ruta de la imagen correspondiente a la clase predicha
+    class_image_path = f"./media/monkeys/{classes[prediction]}.jpg"
+    print(class_image_path)
+    # Verificar si la imagen existe
+    if os.path.exists(class_image_path):
+        # Abrir y mostrar la imagen correspondiente
+        class_image = Image.open(class_image_path)
+        # Redimensionar la imagen para ajustarla al tamaño de visualización
+        class_image = class_image.resize((500, 500), Image.ANTIALIAS)
+        # Convertir la imagen a un formato compatible con Tkinter
+        class_img = ImageTk.PhotoImage(class_image)
+        # Actualizar el widget de imagen de la clase predicha
+        class_image_label.config(image=class_img)
+        class_image_label.image = class_img  # Mantener una referencia para evitar que se recolecte el objeto
+    else:
+        messagebox.showwarning("Advertencia", "No se encontró una imagen correspondiente a esta clase.")
 
 # Configuración de la interfaz gráfica
 root = tk.Tk()
 root.title("Clasificación de Monos")
 
 choose_button = tk.Button(root, text="Seleccionar Imagen", command=classify_image)
-choose_button.pack(pady=20)
+choose_button.grid(row=0, column=0, pady=20, padx=20)
 
-image_label = tk.Label(root)
-image_label.pack()
+user_image_label = tk.Label(root)
+user_image_label.grid(row=1, column=0, padx=20)
+
+class_image_label = tk.Label(root)
+class_image_label.grid(row=1, column=1, padx=20)
 
 result_label = tk.Label(root, text="Predicción: ")
-result_label.pack(pady=10)
+result_label.grid(row=2, column=0, columnspan=2, pady=10)
 
 root.mainloop()
-
-
 
