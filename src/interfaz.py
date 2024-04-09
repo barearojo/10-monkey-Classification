6d4bdd1a78dd54a446abd2cd799d8b9779834e5b
barearojo <barearojojuan@gmail.com>
2024-04-09 15:28:09 +0200

    tk basico 2

--- src/interfaz.py
+++ src/interfaz.py
@@ -38,24 +38,37 @@ def classify_image():
     file_path = filedialog.askopenfilename()
     if file_path:
         try:
-            print(file_path)
+            image = Image.open(file_path)
             prediction = classify(mejor_modelo, image_resize, file_path)
             result_label.config(text=f"Predicción: {classes[prediction]}")
+            # Mostrar la imagen cargada
+            display_image(image)
         except Exception as e:
             messagebox.showerror("Error", f"Error al procesar la imagen: {str(e)}")
 
-
+def display_image(image):
+    # Redimensionar la imagen para ajustarla al tamaño de visualización
+    image = image.resize((700, 700), Image.ANTIALIAS)
+    # Convertir la imagen a un formato compatible con Tkinter
+    img = ImageTk.PhotoImage(image)
+    # Actualizar el widget de imagen
+    image_label.config(image=img)
+    image_label.image = img  # Mantener una referencia para evitar que se recolecte el objeto
 
 # Configuración de la interfaz gráfica
 root = tk.Tk()
-root.title("Clasificación de Imágenes")
+root.title("Clasificación de Monos")
 
 choose_button = tk.Button(root, text="Seleccionar Imagen", command=classify_image)
 choose_button.pack(pady=20)
+
+image_label = tk.Label(root)
+image_label.pack()
 
 result_label = tk.Label(root, text="Predicción: ")
 result_label.pack(pady=10)
 
 root.mainloop()
+
 
 
